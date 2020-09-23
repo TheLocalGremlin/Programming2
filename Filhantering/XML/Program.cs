d94b9a856619034f1604b2f32a807afe412f5ad4
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-09-23 10:15:09 +0200

    First Commit

---
+++ Filhantering/XML/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            XmlSerializer serializer = new XmlSerializer (typeof(PowerUp);
+            FileStream myFile = File.Open("PowerUp.xml", FileMode.OpenOrCreate);
+
+            PowerUp p = (PowerUp) serializer.Deserialize(myFile);
+        }
+    }
+}
---
+++ xmlUppgift/xmlUppgift/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xmlUppgift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
