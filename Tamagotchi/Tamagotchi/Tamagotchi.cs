adb4ba50e00a64e27b6ddb31c10247aa2101046e
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-10-06 11:08:38 +0200

    First Commit (Tamagotchi)

---
+++ Tamagotchi/Tamagotchi/Tamagotchi.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Tamagotchi
+{
+    public class Tamagotchi
+    {
+        int hunger;
+        int boredom;
+
+        List<string> words = new List<string>();
+
+        Random generator = new Random();
+
+        public string name;
+    }
+}
