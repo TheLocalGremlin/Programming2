359396c3c317a7610448048e764c9d8dae6caa66
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-10-13 22:31:52 +0200

    Tamagotchi Complete

--- Tamagotchi/Tamagotchi/Program.cs
+++ Tamagotchi/Tamagotchi/Program.cs
@@ -6,7 +6,76 @@ namespace Tamagotchi
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            bool alive = true;
+            bool understood = false;
+
+            string answer;
+
+            Tamagotchi newTamagotchi = new Tamagotchi();
+
+            Console.WriteLine("Name your tomagatchi:");
+            newTamagotchi.name = Console.ReadLine();
+
+            while (alive == true)
+            {
+                Console.Clear();
+
+                understood = false;
+
+                newTamagotchi.Tick();
+
+                alive = newTamagotchi.GetAlive();
+
+                System.Console.WriteLine("What would you like to do?");
+                System.Console.WriteLine("1: Say hi to your tamagotchi");
+                System.Console.WriteLine("2: See your tamagotchi's stats");
+                System.Console.WriteLine("3: Feed your tamagotchi");
+                System.Console.WriteLine("4: Teach your tamagotchi a new word");
+                System.Console.WriteLine("5: Nothing");
+
+                while (understood == false)
+                {
+                    answer = Console.ReadLine();
+
+                    switch(answer.ToLower())
+                    {
+                        case "1":
+                            understood = true;
+                            newTamagotchi.Hi();
+                            break;
+
+                        case "2":
+                            understood = true;
+                            newTamagotchi.PrintStats();
+                            break;
+
+                        case "3":
+                            understood = true;
+                            newTamagotchi.Feed();
+                            break;
+
+                        case "4":
+                            understood = true;
+                            System.Console.WriteLine("What word would you like to teach " + newTamagotchi.name + "?");
+                            string word = Console.ReadLine();
+                            newTamagotchi.Teach(word);
+                            break;
+
+                        case "5":
+                            understood = true;
+                            break;
+
+                        default:
+                            System.Console.WriteLine("Sorry, I didn't understand that.");
+                            break;
+                    }
+
+                    Console.ReadLine();
+                }
+            }
+
+            System.Console.WriteLine(newTamagotchi.name + " has died.");
+            Console.ReadLine();
         }
     }
 }
--- Tamagotchi/Tamagotchi/Tamagotchi.cs
+++ Tamagotchi/Tamagotchi/Tamagotchi.cs
@@ -1,16 +1,102 @@
 using System;
+using System.Collections.Generic;
 
 namespace Tamagotchi
 {
     public class Tamagotchi
     {
-        int hunger;
-        int boredom;
+        int hunger = 0;
+        int boredom = 0;
 
-        List<string> words = new List<string>();
+        public string name;
+
+        List<string> words = new List<string>() {" ",};
+
+        bool isAlive = true;
 
         Random generator = new Random();
 
-        public string name;
+        public bool GetAlive()
+        {
+            if (isAlive == false)
+            {
+                return false;
+            }
+
+            else
+            {
+                return true;
+            }
+        }
+
+        public void Tick()
+        {
+            hunger ++;
+            boredom ++;
+
+            if (hunger >= 10 && boredom >= 10)
+            {
+                isAlive = false;
+            }
+        }
+
+        public void ReduceBoredom()
+        {
+            boredom =- 2;
+        }
+
+        public void Feed()
+        {
+            System.Console.WriteLine("You give " + name + " some food.");
+            System.Console.WriteLine("They look happy!");
+            hunger =- 2;
+        }
+        public void Hi()
+        {
+            int n = generator.Next(words.Count);
+            System.Console.WriteLine("Hello! " + words[n] + "!");
+
+            ReduceBoredom();
+        }
+
+        public void Teach(string word)
+        {
+            System.Console.WriteLine("You've taught " + name + " how to say " + word + "!");
+            words.Add(word);
+            ReduceBoredom();
+        }
+
+        public void PrintStats()
+        {
+            System.Console.WriteLine("Hunger: " + hunger);
+            System.Console.WriteLine("Boredom: " + boredom);
+
+            int mood = hunger + boredom;
+
+            if (mood <= 3)
+            {
+                System.Console.WriteLine(name + " is feeling very happy!");
+            }
+
+            else if (mood <=4 && mood >= 7)
+            {
+                System.Console.WriteLine(name + " is feeling happy!");
+            }
+
+            else if (mood <= 8 && mood >= 11)
+            {
+                System.Console.WriteLine(name + " is feeling neutral!");
+            }
+
+            else if (mood <= 12 && mood >= 15)
+            {
+                System.Console.WriteLine(name + " is feeling bad!");
+            }
+
+            else if (mood >= 16)
+            {
+                System.Console.WriteLine(name + " is feeling very bad!");
+            }
+        }
     }
 }
