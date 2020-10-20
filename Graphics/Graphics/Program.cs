bd6316e5f300c797d2658ff5a021ca7cd668fdd9
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-10-20 10:15:54 +0200

    Raylib 2

--- Graphics/Graphics/Program.cs
+++ Graphics/Graphics/Program.cs
@@ -8,7 +8,16 @@ namespace Graphics
         static void Main(string[] args)
         {
             Raylib.InitWindow(800, 600, "Aloha Brochaskis");
-            
+
+            while (!Raylib.WindowSouldClose())
+            {
+                Raylib.BeginDrawing();
+
+                Raylib.ClearBackground()
+
+                Raylib.EndDrawing();
+            }
+
         }
     }
 }
