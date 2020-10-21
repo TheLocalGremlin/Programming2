293348c87c8a818c61a296a3beacf733c77c69f8
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-10-21 08:53:18 +0200

    Raylib 3

--- Graphics/Graphics/Program.cs
+++ Graphics/Graphics/Program.cs
@@ -9,11 +9,40 @@ namespace Graphics
         {
             Raylib.InitWindow(800, 600, "Aloha Brochaskis");
 
+            Color porble = new Color(100, 25, 148, 255);
+            Color mint = new Color(51, 255, 204, 255);
+
+            float x = 0;
+            float y = 0;
+
             while (!Raylib.WindowShouldClose())
             {
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_RIGHT))
+                {
+                    x += 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_LEFT))
+                {
+                    x -= 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_DOWN))
+                {
+                    y += 0.1f;
+                }
+
+                if (Raylib.IsKeyDown(KeyboardKey.KEY_UP))
+                {
+                    y -= 0.1f;
+                }
+
+
                 Raylib.BeginDrawing();
 
-                Raylib.ClearBackground(Color.PURPLE);
+                Raylib.DrawRectangle((int)x, (int)y, 10, 10, mint);
+
+                Raylib.ClearBackground(porble);
                 
                 Raylib.EndDrawing();
             }
