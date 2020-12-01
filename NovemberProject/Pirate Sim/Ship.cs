02101750e1f923fcdeb09ceb2baa6687860e088f
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2020-12-01 11:09:17 +0100

    Do some more shit next time

--- NovemberProject/Pirate Sim/Ship.cs
+++ NovemberProject/Pirate Sim/Ship.cs
@@ -4,6 +4,15 @@ namespace Pirate Sim
 {
     public class Ship
     {
+        int HP = 25;
+        int DMG = 5;
+
+
+        Random genarator = new Random();
+    }
+
+    public void Attack()
+    {
         
     }
 }
