094731340465861924c778540d65c43344c3c091
TheLocalGremlin <70197668+TheLocalGremlin@users.noreply.github.com>
2021-04-20 10:51:52 +0200

    First Commit

---
+++ Prog2Slutprojekt/MonsterHunterSim/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace MonsterHunterSim
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+
+            System.Console.WriteLine("What is your name?");
+            player.username = Console.ReadLine();
+            if (String.IsNullOrEmpty(player.username))
+            {
+                player.username = "Guild Master";
+            }
+
+            Console.ReadLine();
+
+            //while
+        }
+    }
+}
